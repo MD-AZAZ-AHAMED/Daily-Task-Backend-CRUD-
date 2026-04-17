0f2da96fec1cdecb3d9c5bd6fb36de4d874e8add
MD. AZAZ AHAMED <68296908+MD-AZAZ-AHAMED@users.noreply.github.com>
2026-04-17 23:08:08 +0600

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose =require("mongoose");
 
 async function connectDB(){
-    await mongoose.connect("mongodb://yt:Zley7f4G7Hzze4w4@ac-sbirgdl-shard-00-00.snljk2x.mongodb.net:27017,ac-sbirgdl-shard-00-01.snljk2x.mongodb.net:27017,ac-sbirgdl-shard-00-02.snljk2x.mongodb.net:27017/?ssl=true&replicaSet=atlas-bfzpv6-shard-0&authSource=admin&appName=yt-complete-backend/halley")
+    await mongoose.connect("mongodb://yt:<Password>@ac-sbirgdl-shard-00-00.snljk2x.mongodb.net:27017,ac-sbirgdl-shard-00-01.snljk2x.mongodb.net:27017,ac-sbirgdl-shard-00-02.snljk2x.mongodb.net:27017/?ssl=true&replicaSet=atlas-bfzpv6-shard-0&authSource=admin&appName=yt-complete-backend/halley")
 
     console.log("connected to DB")
 }
